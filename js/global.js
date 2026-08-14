dabca17e48daef1285a7564ff4a96e9fde43f3a5
PatrickHuijs <152437425+PatrickHuijs@users.noreply.github.com>
2026-08-14 09:14:54 +0200

    Update global.js
    anchore links

--- js/global.js
+++ js/global.js
@@ -29,6 +29,32 @@
     });
   });
 
+//<!-- LENIS ANCHOR LINK -->
+// Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Scroll-To Anchor Lenis
+function initScrollToAnchorLenis() {
+  document.querySelectorAll("[data-anchor-target]").forEach(element => {
+    element.addEventListener("click", function () {
+      const targetScrollToAnchorLenis = this.getAttribute("data-anchor-target");
+
+      lenis.scrollTo(targetScrollToAnchorLenis, {
+        easing: (x) => (x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2),
+        duration: 1.2,
+        offset: 0 // Option to create an offset when there is a fixed navigation for example
+      });
+    });
+  });
+}
+
+// Initialize Scroll-To Anchor Lenis
+document.addEventListener('DOMContentLoaded', () => {
+  initScrollToAnchorLenis();
+});
+
 
 // <!--  LOGO WALL -->
 
